d6b5e40e29c21dd30e6e68885a64da49a066e3bb
Ayushpanchal26 <113408381+Ayushpanchal26@users.noreply.github.com>
2022-10-04 21:39:57 +0530

    Add files via upload

---
+++ useofSigns.C
@@ -0,0 +1,22 @@
+// If a number is greater than 9 & less than 100:
+
+#include<math.h>
+
+int main(){
+  
+         
+          int x;
+     
+          printf("Enter a number:");
+ 
+          scanf("%d",&x);
+      
+          printf("%d",x>9 && x<100);
+  
+  
+return 0;
+   
+ 
+}
+
+// if output is 1 it means that the number is b/w 9and100.
